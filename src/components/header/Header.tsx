089e7c0443fdb4e9c2b4e6f8f385587c7f04163a
misterildar <misterildar@gmail.com>
2025-08-02 16:39:28 +0300

    fix data

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,12 +9,12 @@ const Header = () => {
     <section className={styles.header}>
         <Image className={styles.logo} alt="logo" width={344} height={73} src={logoTT}/>
         <div className={styles.links}>
-            <a className={styles.link} href={""}>Home</a>
-            <a className={styles.link} href={""}>How It Works</a>
-            <a className={styles.link} href={""}>Calculator</a>
-            <a className={styles.link} href={""}>FAQ</a>
-            <a className={styles.link} href={""}>About us</a>
-            <a className={styles.link} href={""}>Contacts</a>
+            <Link className={styles.link} href={""}>Home</Link>
+            <Link className={styles.link} href={""}>How It Works</Link>
+            <Link className={styles.link} href={""}>Calculator</Link>
+            <Link className={styles.link} href={""}>FAQ</Link>
+            <Link className={styles.link} href={""}>About us</Link>
+            <Link className={styles.link} href={""}>Contacts</Link>
         </div>
     </section>  
   )
